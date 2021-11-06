1a207e9fd75873ea8a8b1dc4e718e26dd8b46250
Rendxnn <88605945+Rendxnn@users.noreply.github.com>
2021-11-06 15:52:20 -0500

    Update Principal.java
    Avanzado hasta el case 7.

--- Principal.java
+++ Principal.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Principal
 {
@@ -5,22 +6,22 @@ public class Principal
     {
         Principal.mostrarMenu();
     }
-    
+
     public static void mostrarMenu(){
         Scanner sc = new Scanner(System.in);
         while(true)
         {
             System.out.println("- Ingrese 0 si desea finalizar la ejecución del programa." + "\n"
-            + "- Ingrese 1 para crear un nuevo vehiculo con su modelo, marca, valor comercial y color." + "\n" 
-            + "- Ingrese 2 para mostrar los vehiculos almacenados." + "\n"
-            + "- Ingrese 3 para mostrar la cantidad de vehiculos creados." + "\n"
-            + "- Ingrese 4 para mostrar la informacion de vehiculos verdes." + "\n"
-            + "- Ingrese 5 para mostrar los modelos vehiculos entre 2000 y 2021." + "\n"
-            + "- Ingrese 6 para crear un nuevo sensor con su tipo y su valor." + "\n"
-            + "- Ingrese 7 para mostrar los sensores almacenados." + "\n" 
-            + "- Ingrese 8 para mostrar la cantidad de sensores creados." + "\n"
-            + "- Ingrese 9 para mostrar los sensores de tipo 'temperatura'." + "\n"
-            + "- Ingrese 666 para mostrar los sensores tipo 'temperatura' ordenados por valor.");
+                    + "- Ingrese 1 para crear un nuevo vehiculo con su modelo, marca, valor comercial y color." + "\n"
+                    + "- Ingrese 2 para mostrar los vehiculos almacenados." + "\n"
+                    + "- Ingrese 3 para mostrar la cantidad de vehiculos creados." + "\n"
+                    + "- Ingrese 4 para mostrar la informacion de vehiculos verdes." + "\n"
+                    + "- Ingrese 5 para buscar un vehículo por su id." + "\n"
+                    + "- Ingrese 6 para crear un nuevo sensor con su tipo y su valor a un vehículo." + "\n"
+                    + "- Ingrese 7 para mostrar los sensores almacenados de un vehículo." + "\n"
+                    + "- Ingrese 8 para mostrar la cantidad de sensores creados." + "\n"
+                    + "- Ingrese 9 para mostrar los sensores de tipo 'temperatura'." + "\n"
+                    + "- Ingrese 666 para mostrar los sensores tipo 'temperatura' ordenados por valor.");
             int num = sc.nextInt();
             if(num == 0)
             {
@@ -42,22 +43,22 @@ public class Principal
                         System.out.println("Ingrese el valor comercial del vehiculo: ");
                         double valorComercial = sc.nextDouble();
                         System.out.println("¿Desea ingresar el color del vehiculo?"+ "\n"
-                        + "- Ingrese 'Si' para ingresar un color."+"\n"
-                        + "- Ingrese 'No' y el color por defecto será verde.");
+                                + "- Ingrese 'Si' para ingresar un color."+"\n"
+                                + "- Ingrese 'No' y el color por defecto será verde.");
                         String siNo = sc.next();
                         if(siNo.equals("Si"))
                         {
                             System.out.println("Ingrese el color del vehiculo: ");
                             String color = sc.next();
-                            Vehiculo.vehiculos[Vehiculo.posAnadir] = new Vehiculo(modelo, marca, valorComercial, color);
+                            Vehiculo vehiculo = new Vehiculo(modelo, marca, valorComercial, color);
                         }else if(siNo.equals("No"))
                         {
-                            Vehiculo.vehiculos[Vehiculo.posAnadir] = new Vehiculo(modelo, marca, valorComercial);
+                            Vehiculo vehiculo = new Vehiculo(modelo, marca, valorComercial);
                         }
                         Vehiculo.posAnadir++;
                         System.out.println("¿Desea ingresar otro vehiculo?"+ "\n"
-                        + "- Ingrese 'Si' para crear un nuevo vehiculo."+"\n"
-                        + "- Ingrese 'No' para volver al menú.");
+                                + "- Ingrese 'Si' para crear un nuevo vehiculo."+"\n"
+                                + "- Ingrese 'No' para volver al menú.");
                         String siNo2 = sc.next();
                         if(siNo2.equals("Si"))
                         {
@@ -72,55 +73,56 @@ public class Principal
                     System.out.println(Vehiculo.toStringVehiculos());
                     break;
                 case 3:
-                    System.out.println(Vehiculo.cantidadVehiculos());
+                    System.out.println("La cantidad de vehículos creados es: " + Vehiculo.cantidadVehiculos());
                     break;
                 case 4:
                     for(int i = 0; i<Vehiculo.posAnadir; i++)
                     {
-                        if(Vehiculo.vehiculos[i].getColor().equals("verde"))
+                        if(Vehiculo.vehiculos.get(i).getColor().equals("verde"))
                         {
-                            System.out.println(Vehiculo.vehiculos[i].toString());
+                            System.out.println(Vehiculo.vehiculos.get(i).toString());
                         }
                     }
                     break;
                 case 5:
-                    for(int i = 0; i<Vehiculo.posAnadir; i++)
-                    {
-                        if((Vehiculo.vehiculos[i].getModelo()>=2000)&&(Vehiculo.vehiculos[i].getModelo()<=2021))
-                        {
-                            System.out.println(Vehiculo.vehiculos[i].toString());
-                        }
+                    System.out.println("Ingrese id a buscar. ");
+                    int idscan = sc.nextInt();
+                    if (Vehiculo.obtenerVehiculoPorId(idscan) == null) {
+                        System.out.println("No se ha encontrado un vehículo por ese id");
+                        break;
+                    }
+                    else {
+                        System.out.println(Vehiculo.obtenerVehiculoPorId(idscan).toString());
                     }
                     break;
                 case 6:
-                    while(true){
-                        if(Sensor.posAnadir == 8)
-                        {
-                            System.out.println("Error, base de datos llena");
-                            break;
-                        }
-                        System.out.println("Ingrese el tipo de sensor: ");
+                    System.out.println("Ingrese id a buscar. ");
+                    int idscan2 = sc.nextInt();
+                    if (Vehiculo.obtenerVehiculoPorId(idscan2) == null) {
+                        System.out.println("No se ha encontrado un vehículo por ese id");
+                        break;
+                    }
+                    else {
+                        System.out.println("Ingrese tipo de sensor. ");
                         String tipo = sc.next();
-                        System.out.println("Ingrese el valor del sensor: ");
+                        System.out.println("Ingrese valor del sensor. ");
                         double valor = sc.nextDouble();
-                        Sensor.sensores[Sensor.posAnadir] = new Sensor(tipo, valor);
-                        Sensor.posAnadir++;
-                        System.out.println("¿Desea ingresar otro sensor?"+ "\n"
-                        + "- Ingrese 'Si' para crear un nuevo sensor."+"\n"
-                        + "- Ingrese 'No' para volver al menú.");
-                        String siNo = sc.next();
-                        if(siNo.equals("Si"))
-                        {
-                            continue;
-                        }else if(siNo.equals("No"))
-                        {
-                            break;
+                        Sensor sensor = new Sensor(tipo, valor);
+                        Vehiculo.obtenerVehiculoPorId(idscan2).anadirSensor(sensor);
+                        break;
+                    }
+                case 7:
+                    System.out.println("Ingrese id a buscar");
+                    int id7  = sc.nextInt();
+                    if (Vehiculo.obtenerVehiculoPorId(id7) == null) {
+                        System.out.println("No se ha encontrado ningún vehículo con ese id.");
+                    }
+                    else {
+                        ArrayList<Sensor> arr = Vehiculo.obtenerVehiculoPorId(id7).getSensores();
+                        for (Sensor sensor : arr) {
+                            System.out.println(sensor.toString());
                         }
                     }
-                    break;
-                case 7:
-                    System.out.println(Sensor.toStringSensores());
-                    break;
                 case 8:
                     System.out.println(Sensor.cantidadSensores());
                     break;
