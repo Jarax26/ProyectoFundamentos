cb594ce404929751eada93cdc6742c3a49b14eb3
Tomás <83550183+Jarax26@users.noreply.github.com>
2021-11-06 18:58:07 -0500

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -6,12 +6,12 @@ public class Sensor
     public static int cantSensorTemp = 0;
     private String tipo;
     private double valor;
-    
+
     public Sensor()
     {
-    
+
     }
-    
+
     public Sensor(String t, double v)
     {
         this.tipo = t;
@@ -21,32 +21,32 @@ public class Sensor
             cantSensorTemp+=1;
         }
     }
-    
+
     public void setTipo(String t)
     {
         this.tipo = t;
     }
-    
+
     public String getTipo()
     {
         return this.tipo;
     }
-    
+
     public void setValor(double v)
     {
         this.valor = v;
     }
-    
+
     public double getValor()
     {
         return this.valor;
     }
-    
+
     public String toString()
     {
         return "Sensor -- Tipo " + this.tipo + " y valor de " + this.valor;
     }
-    
+
     public static String toStringSensores()
     {
         String texto = "";
@@ -63,14 +63,14 @@ public class Sensor
         }
         return texto1;
     }
-    
+
     public static int cantidadSensores()
     {
         return posAnadir;
     }
-    
+
     public static String toStringOrdenarValores()
-    {    
+    {
         Sensor[]ord = new Sensor[cantSensorTemp];
         int x = -1;
         for(int i = 0; i<ord.length; i++){
@@ -79,7 +79,7 @@ public class Sensor
                 if(sensores[x].getTipo().equals("temperatura"))
                 {
                     ord[i]  = new Sensor(sensores[x].getTipo(), sensores[x].getValor());
-                    break; 
+                    break;
                 }
             }
         }
