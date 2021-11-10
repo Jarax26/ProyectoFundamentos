b48cc11bb5007b0cb6c2cf7168d06bb819222c5a
Rendxnn <88605945+Rendxnn@users.noreply.github.com>
2021-11-09 22:21:02 -0500

    Update BichoNormal.java

--- Minijuego/BichoNormal.java
+++ Minijuego/BichoNormal.java
@@ -1,6 +1,8 @@
 public class BichoNormal extends Bicho{
     public BichoNormal() {
         setSalud(10);
-        setNombre("BN - " + 10);
+        setId("BN - ");
+        setNombre(this.getId() + this.getSalud());
     }
+
 }
